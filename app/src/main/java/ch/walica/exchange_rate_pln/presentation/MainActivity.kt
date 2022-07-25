1097d0f76dfea3946f2e17d69a630a3c407b9f30
Marek Walica <mwalica@gmail.com>
2022-07-25 12:17:06 +0200

    spash screen finish

--- app/src/main/java/ch/walica/exchange_rate_pln/presentation/MainActivity.kt
+++ app/src/main/java/ch/walica/exchange_rate_pln/presentation/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -23,6 +24,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             Exchange_rate_plnTheme {
                 // A surface container using the 'background' color from the theme
